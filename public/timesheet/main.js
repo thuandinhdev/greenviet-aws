75ef0487d126549270940f0369e6c9cb0c76dea9
thuandinhdev <thuandinhdev@gmail.com>
2024-11-12 15:25:31 +0700

    no message

--- public/timesheet/main.js
+++ public/timesheet/main.js
@@ -10143,6 +10143,9 @@ var environment;
 environment = {
     production: false,
     apiUrl: 'https://timesheet.greenviet.net'
+    // apiUrl: 'https://greenviet.cuortech.com'
+    // apiUrl: 'https://greenviet.azurewebsites.net'
+    // apiUrl: 'http://chetsapp.de:8899'
 };
 
 
